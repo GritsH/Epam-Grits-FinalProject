f647e4d9378d36949491d7ca63e0db1e694f516e
Helen <grits.lena@gmail.com>
2022-06-15 17:53:56 +0300

    add controller

---
+++ src/main/java/by/grits/news/controller/CommandProvider.java
@@ -0,0 +1,33 @@
+package by.grits.news.controller;
+
+import by.grits.news.command.Command;
+import by.grits.news.command.CommandType;
+import by.grits.news.command.impl.DefaultCommand;
+import by.grits.news.command.impl.SignupCommand;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.EnumMap;
+
+import static by.grits.news.command.CommandType.*;
+
+public class CommandProvider {
+    private static final Logger LOGGER = LogManager.getLogger(CommandProvider.class);
+
+    private static EnumMap<CommandType, Command> commands = new EnumMap<>(CommandType.class);
+    static{
+        commands.put(DEFAULT, new DefaultCommand());
+        commands.put(SIGNUP, new SignupCommand());
+    }
+    public static Command of(String commandName) {
+        Command currentCommand = commands.get(DEFAULT);
+        if (commandName != null) {
+            try {
+                currentCommand = commands.get(CommandType.valueOf(commandName.toUpperCase()));
+            } catch (IllegalArgumentException e) {
+                LOGGER.error("Command " + commandName + " is not present.", e);
+            }
+        }
+        return currentCommand;
+    }
+}
---
+++ src/main/java/by/grits/news/controller/Controller.java
@@ -0,0 +1,61 @@
+package by.grits.news.controller;
+
+import by.grits.news.command.Command;
+import by.grits.news.command.RequestParameter;
+import by.grits.news.command.Router;
+import by.grits.news.command.exception.CommandException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+@WebServlet(name = "controller", urlPatterns = {"/controller"})
+public class Controller extends HttpServlet {
+    static final Logger LOGGER = LogManager.getLogger();
+
+    public void init() {
+        LOGGER.info("Servlet init: " + this.getServletInfo());
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String commandName = request.getParameter(RequestParameter.COMMAND);
+        Command command = CommandProvider.of(commandName);
+        LOGGER.info(commandName);
+        try {
+            Router router = command.execute(request);
+            String toPage = router.getPage();
+
+            switch (router.getType()) {
+                case FORWARD:
+                    request.getRequestDispatcher(toPage).forward(request, response);
+                    break;
+                case REDIRECT:
+                    response.sendRedirect(toPage);
+                    break;
+                default:
+                    response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            }
+        } catch (CommandException e) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
+    public void destroy() {
+        LOGGER.log(Level.INFO, "Servlet destroyed: " + this.getServletName());
+    }
+}
