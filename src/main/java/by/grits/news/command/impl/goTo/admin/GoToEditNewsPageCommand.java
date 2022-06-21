a759bdcde4c3243bb4285404a907b67b28e648ea
Helen <grits.lena@gmail.com>
2022-06-21 20:35:39 +0300

    minor fixes

--- src/main/java/by/grits/news/command/impl/goTo/admin/GoToEditNewsPageCommand.java
+++ src/main/java/by/grits/news/command/impl/goTo/admin/GoToEditNewsPageCommand.java
@@ -2,15 +2,13 @@ package by.grits.news.command.impl.goTo.admin;
 
 import by.grits.news.command.*;
 import by.grits.news.command.exception.CommandException;
-import by.grits.news.entities.News;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import static by.grits.news.command.SessionAttribute.NEWS_ID_TO_EDIT_SES;
+import static by.grits.news.command.SessionAttribute.NEWS_ID_TO_EDIT_SESSION;
 
 public class GoToEditNewsPageCommand implements Command {
     @Override
@@ -19,7 +17,7 @@ public class GoToEditNewsPageCommand implements Command {
         Map<String, String> newsData = new HashMap<>();
         //List<News> allNews;
         String id = request.getParameter(RequestParameter.NEWS_ID_TO_EDIT);
-        session.setAttribute(NEWS_ID_TO_EDIT_SES, id);
+        session.setAttribute(NEWS_ID_TO_EDIT_SESSION, id);
         session.setAttribute(SessionAttribute.NEWS_DATA_SESSION, newsData);
         String currentPage = Command.extract(request);
         session.setAttribute(SessionAttribute.CURRENT_PAGE, currentPage);
