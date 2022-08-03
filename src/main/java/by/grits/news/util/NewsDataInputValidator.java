0af80ee6a814914edf0a1423f6fe5fdc1b95e339
Helen <grits.lena@gmail.com>
2022-08-03 08:21:52 +0300

    fix regexp

--- src/main/java/by/grits/news/util/NewsDataInputValidator.java
+++ src/main/java/by/grits/news/util/NewsDataInputValidator.java
@@ -4,14 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NewsDataInputValidator {
-    private static final String INPUT_PATTERN = "[A-zА-яЁё\\d !@?.&():,;-]+";
+    private static final String INPUT_PATTERN = "[A-zА-яЁё\\d \\r\\n\\t!@?.&():,;-]+";
     private static final Pattern PATTERN = Pattern.compile(INPUT_PATTERN);
     public static boolean validateInput(String input) {
         Matcher matcher = PATTERN.matcher(input);
         return matcher.matches();
-    }
-
-    public static String makeValid(String input){
-        return input.replaceAll(INPUT_PATTERN, " ");
     }
 }
